e1a01c890b1ab49b26ec23e6884ba84f08907b41
Lance Buckley <erchancy@gmail.com>
2023-08-24 10:38:17 -0500

    brazil invoice query

---
+++ brazil_customers_invoices.sql
@@ -0,0 +1,9 @@
+SELECT (c.FirstName || ' ' || c.LastName) AS FullName, InvoiceId, InvoiceDate, BillingCountry
+    FROM Invoice AS i
+        JOIN Customer AS c
+        ON i.CustomerId = c.CustomerId
+    WHERE c.Country = 'Brazil'
+
+SELECT *
+    FROM Customer
+    WHERE Country = 'Brazil'
