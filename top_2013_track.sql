7c881bf8ffa39230868f6ccc9580b4fb4d0c1490
Lance Buckley <erchancy@gmail.com>
2023-08-24 14:47:45 -0500

    24) get most purchased track of 2013...it was a lot of them

--- top_2013_track.sql
+++ top_2013_track.sql
@@ -1,6 +1,10 @@
-SELECT BillingCountry, MAX(TotalSalesPerCountry) AS TotalSales
-    FROM(
-SELECT BillingCountry, SUM(total) AS TotalSalesPerCountry
-    FROM Invoice
-    GROUP BY BillingCountry
-    );
+Provide a query that shows the most purchased track(s) of 2013.
+
+SELECT t.name AS Track, SUM(il.Quantity) AS Sales
+    FROM Invoice AS i
+    JOIN InvoiceLine AS il
+        ON i.InvoiceId = il.InvoiceId
+    JOIN Track AS t
+        ON il.TrackId = t.TrackId
+    WHERE (i.InvoiceDate BETWEEN '2013-01-01' AND '2013-12-31')
+    GROUP BY il.TrackId
